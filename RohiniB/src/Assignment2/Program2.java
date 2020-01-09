03d52441e1c84477a30dcd1b72cd5b7f7609d779
RohiniB <bansoderohini06@gmail.com>
2020-01-10 01:09:25 +0530

    Adding Assignment 2

---
+++ RohiniB/src/Assignment2/Program2.java
@@ -0,0 +1,28 @@
+package Assignment2;
+
+public class Program2 {
+
+	   void findDuplicate(int n[])
+	   {
+		   for(int i=0;i<n.length;i++)
+		   {
+			   for(int j=i+1;j<n.length;j++)
+			   {
+				   if(n[i]==n[j])
+				   System.out.println("Duplicate number is :"+n[i]);
+				    
+				}
+			}
+		}
+		
+		 public static void main(String[] args) {
+			int[] num= {0,1,2,3,4,5,1,9};
+			Program2 pgrm = new Program2();
+			pgrm.findDuplicate(num);
+			
+	   }
+		
+}
+
+
+
